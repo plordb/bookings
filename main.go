ccf8db303c6958812ba52c51037d9349318b0705
Pablo <pablo@debian.MiRed>
2023-05-07 00:41:34 -0600

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,60 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-// 02-09
+	"github.com/plordb/bookings/helpers"
+)
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+// 02-10
 
 func main() {
-	dog := Dog{
-		Name:  "Samson",
-		Breed: "German Shepherd",
-	}
+	log.Println("Hello")
 
-	PrintInfo(dog)
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	myVar.TypeNumber = 5
 
-	gorilla := Gorilla{
-		Name:          "King Kong",
-		Color:         "Black",
-		NumberOfTeeth: 12,
-	}
-
-	PrintInfo(gorilla)
-
-}
-
-func (d Dog) Says() string {
-	return "woof"
-}
-
-func (d Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (d Gorilla) Says() string {
-	return "grunt"
-}
-
-func (d Gorilla) NumberOfLegs() int {
-	return 2
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
-
+	log.Println(myVar.TypeName)
+	log.Println(myVar.TypeNumber)
 }
