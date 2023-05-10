43d10f67556032f46599feff48d03a9fccd02869
Pablo <pablo@debian.MiRed>
2023-05-09 22:07:10 -0600

    Siguiente Commit

--- main_test.go
+++ main_test.go
@@ -35,3 +35,18 @@ func TestDivision(t *testing.T) {
 		}
 	}
 }
+
+/*
+func TestDivide(t *testing.T) {
+	_, err := divide(10.0, 1.0)
+	if err != nil {
+		t.Error("Got an error when weshould not have")
+	}
+}
+func TestBadDivide(t *testing.T) {
+	_, err := divide(10.0, 0.0)
+	if err == nil {
+		t.Error("Did not get an error when we sould have")
+	}
+}
+*/
