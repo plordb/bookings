129d30012ea808b9abf63baf48f7c09b489160e5
Pablo <pablo@debian.MiRed>
2023-05-09 20:29:58 -0600

    Siguiente Commit

--- main.go
+++ main.go
@@ -5,18 +5,11 @@ import (
 	"log"
 )
 
-// 02-13
-
-type Person struct {
-	FirstName string `json:"first_name"`
-	LasttName string `json:"last_name"`
-	HairColor string `json:"hair_color"`
-	HasDog    bool   `json:has_dog`
-}
+// 02-14
 
 func main() {
 
-	result, err := divide(100.0, 0.0)
+	result, err := divide(100.0, 10.0)
 	if err != nil {
 		log.Println(err)
 
@@ -30,9 +23,9 @@ func divide(x float32, y float32) (float32, error) {
 	var result float32
 
 	if y == 0 {
-		return result, errors.New(("cannot divde by 0"))
+		return result, errors.New(("cannot divide by 0"))
 	}
 
 	result = x / y
-	return result, errors.New("")
+	return result, nil
 }
