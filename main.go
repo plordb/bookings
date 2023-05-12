97120df2e4084bfcc1c10d0a705b23ecc9fdb699
Pablo <pablo@debian.MiRed>
2023-05-11 18:08:17 -0600

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-// 03-03
+// 03-04
 
 const portNumber = ":8080"
 
@@ -15,8 +16,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 
-	sum := AddValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the About page and 2 + 2 is %d", sum))
+	sum := AddValues(2, 3)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the About page and 2 + 3 is %d", sum))
 }
 
 func AddValues(x int, y int) int {
@@ -24,10 +25,36 @@ func AddValues(x int, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x float32, y float32) (float32, error) {
+
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+
+		return 0, err
+	}
+
+	result := x / y
+
+	return result, nil
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
