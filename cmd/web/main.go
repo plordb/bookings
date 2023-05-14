4019996a7693c5c2791184fc077a98e6f9186300
Pablo <pablo@debian.MiRed>
2023-05-14 15:06:15 -0600

    Siguiente Commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,19 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/plordb/bookings/pkg/config"
 	"github.com/plordb/bookings/pkg/handlers"
 	"github.com/plordb/bookings/pkg/render"
 )
 
-// 04-03
+// 05-01
 
 const portNumber = ":8080"
+const SessionHoras = 24
+
+var app config.AppConfig
+var session *scs.SessionManager
 
 func main() {
 
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = SessionHoras * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
