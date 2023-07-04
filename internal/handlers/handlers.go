71267264f3e2c6890049b91d00fb125f699b1d24
Pablo Lorenzo <plorenzo17@gmail.com>
2023-07-04 10:56:33 -0600

    16-09: Prootecting our routes with authentication middleware

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -494,3 +494,7 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
+
+func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-dashboard.gohtml", &models.TemplateData{})
+}
